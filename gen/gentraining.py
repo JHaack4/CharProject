fff9847384e99649cbae0d06ef3125275acf10c4
JHaack4 <jhaack@g.hmc.edu>
2018-12-08 18:52:34 -0800

    update to character generation, including test char generation

--- gen/gentraining.py
+++ gen/gentraining.py
@@ -4,21 +4,18 @@ import numpy as np
 import cv2
 import random
 
-H = 48 # fixed height
-W = 72 # fixed width
-
-def crop_width(img):
+def crop_width(img, W, H):
     if img.shape[1] > W:
         x = int((img.shape[1] - W)/2)
         return img[0:H, x:x+W] 
     return img
 
-def resize_preserve_aspect_ratio(img):
+def resize_preserve_aspect_ratio(img, W, H):
     w,h = img.shape[1],img.shape[0]
     resized = cv2.resize(img, (int(float(w)/h*H), H))
-    return crop_width(resized)
+    return crop_width(resized,W,H)
 
-def pad_image(img, center=0.5):
+def pad_image(img, W, H, center=0.5):
     """ center in [0,1] determines how the image is centered within the padding """
     if img.shape[1] >= W:
         return img
@@ -27,10 +24,10 @@ def pad_image(img, center=0.5):
     left_pad = total_pad - right_pad
     return cv2.copyMakeBorder( img, 0, 0, left_pad, right_pad, cv2.BORDER_CONSTANT)
 
-def crop_pad_img(img, center=0.5):
+def crop_pad_img(img, W, H,center=0.5):
     t1,b1,l1,r1 = tightest_indices(img)
     cropped = img[0:H, max(0,l1-2):min(img.shape[1],r1+3)]
-    return pad_image(crop_width(cropped))
+    return pad_image(crop_width(cropped,W,H),W,H)
     
 
 def tightest_indices(img):
@@ -48,7 +45,7 @@ def tightest_indices(img):
     right_i = maxs[1]
     return (top_i,bottom_i,left_i,right_i)
 
-def tightest_crop(img):
+def tightest_crop(img,H):
     t1,b1,l1,r1 = tightest_indices(img)
     return img[0:H, max(0,l1):min(img.shape[1],r1+1)]
 
@@ -66,15 +63,15 @@ def overlap_concat(img1, img2, overlap_amt):
     return np.concatenate((left, mid, right), axis = 1), overlapping
 
 
-def get_concat(img1, img2, max_over):
+def get_concat(img1, img2, pixel_overlap, max_join=7, min_slide_back=1, max_slide_back=10):
 
     for i in range(1, min(len(img1[0]), len(img2[0]))):
         new, overlap = overlap_concat(img1, img2, i)
-        if overlap >= max_over or i > 12: # max_overlap is capped at 12
+        if overlap >= pixel_overlap or i > max_join: # max_overlap is capped at 12
             if i == 1:
                 new, _ = overlap_concat(img1, img2, 1)
             else:
-                slide_back = random.randrange(1, 10)
+                slide_back = random.randrange(min_slide_back, max_slide_back)
                 overlap = max(1, i - slide_back)
                 new, _ = overlap_concat(img1, img2, overlap)
             return new
@@ -82,21 +79,9 @@ def get_concat(img1, img2, max_over):
     return new
 
 
-# load data
-letter_path = "../data/letter/"
-letter_files = [f for f in listdir(letter_path) if isfile(join(letter_path, f))]
-letter_keys = [f[len(f)-5] for f in letter_files]
+def generate_training_example(training_images, training_labels, character_set=None,
+                 W=72, H=48, full_widths_only=False, pixel_overlap=10, min_spacing=1, max_join=9):
 
-training_images = []
-for f in letter_files:
-    img = crop_width(tightest_crop(pad_image(resize_preserve_aspect_ratio(cv2.imread(letter_path + f)))))
-    _,img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-    training_images.append(img)
-# todo: split into train/test letters
-print("there are %d traning examples" % len(training_images))
-
-
-def generate_training_example(training_images, training_labels, full_widths_only=False, max_overlap=20):
     target_width = W if full_widths_only else int(random.random()*W)
     target_width = max(4, target_width)
 
@@ -111,6 +96,13 @@ def generate_training_example(training_images, training_labels, full_widths_only
         cur_img = training_images[next_char_idx]
         cur_label = training_labels[next_char_idx]
 
+        # ensure selection from our character set
+        if character_set is not None and len(character_set)>0:
+            while cur_label not in character_set:
+                next_char_idx = random.randint(0, len(training_images)-1)
+                cur_img = training_images[next_char_idx]
+                cur_label = training_labels[next_char_idx]
+
         w = cur_img.shape[1]
 
         if img is None:
@@ -120,7 +112,8 @@ def generate_training_example(training_images, training_labels, full_widths_only
             chars_list.append(cur_label)
             start_list.append(round(phase_offset/w,2))
         else: 
-            img = tightest_crop(get_concat(img, cur_img, max_overlap))
+            img = tightest_crop(get_concat(img, cur_img, pixel_overlap,
+                     max_join=max_join, min_slide_back=min_spacing),H)
 
             chars_list.append(cur_label)
             start_list.append(0.00)
@@ -134,30 +127,41 @@ def generate_training_example(training_images, training_labels, full_widths_only
         else:
             stop_list.append(1.00)
 
-    img = pad_image(img)
+    img = pad_image(img,W,H)
     _,img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
     return (img, chars_list, start_list, stop_list)
 
-for i in range(10):
-    img,char_list,start_list,stop_list = generate_training_example(training_images, letter_keys, False)
-    print(char_list)
-    print(start_list)
-    print(stop_list)
 
-    cv2.imshow('image',img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-""" 
-img = cv2.imread(letter_path + letter_files[1])
-img1 = cv2.imread(letter_path + letter_files[10])
-resized_image = resize_preserve_aspect_ratio(img)
-padded_image = pad_image(resized_image)
-padded_image1 = pad_image(resize_preserve_aspect_ratio(img1))
 
-print(np.shape(padded_image))
-print(np.shape(padded_image1))
+def generate_test_example(test_images, test_labels, character_set=None, min_chars=2, max_chars=6,
+                 H=48, full_widths_only=False, pixel_overlap=10, min_spacing=1, max_join=9):
 
-i = get_concat(padded_image, padded_image1, 10)
-cv2.imshow('image',i)
-cv2.waitKey(0)
-cv2.destroyAllWindows() """
+    chars_list = []
+    img = None
+    num_chars = random.randint(min_chars, max_chars)
+
+    for _ in range(num_chars):
+
+        next_char_idx = random.randint(0, len(test_images)-1)
+        cur_img = test_images[next_char_idx]
+        cur_label = test_labels[next_char_idx]
+
+        # ensure selection from our character set
+        if character_set is not None and len(character_set)>0:
+            while cur_label not in character_set:
+                next_char_idx = random.randint(0, len(test_images)-1)
+                cur_img = test_images[next_char_idx]
+                cur_label = test_labels[next_char_idx]
+
+        w = cur_img.shape[1]
+        chars_list.append(cur_label)
+
+        if img is None:
+            img = cur_img[0:H, 0:w]
+        else: 
+            img = tightest_crop(get_concat(img, cur_img, pixel_overlap,
+                     max_join=max_join, min_slide_back=min_spacing),H)
+
+    img = tightest_crop(pad_image(img,img.shape[1]+50,H),H)
+    _,img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+    return (img, chars_list)
---
+++ gen/loadchars.py
@@ -0,0 +1,45 @@
+
+import cv2
+from gentraining import *
+
+H = 48
+W = 72
+character_set = set(['A','T','O','X'])
+
+# load data
+letter_path = "../data/letter/"
+letter_files = [f for f in listdir(letter_path) if isfile(join(letter_path, f))]
+letter_keys = [f[len(f)-5] for f in letter_files]
+
+training_images = []
+for f in letter_files:
+    img = crop_width(tightest_crop(pad_image(resize_preserve_aspect_ratio(cv2.imread(letter_path + f),W,H),W,H),H),W,H)
+    _,img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+    training_images.append(img)
+# todo: split into train/test letters
+print("there are %d traning examples" % len(training_images))
+
+
+
+for i in range(10):
+    img,char_list,start_list,stop_list = generate_training_example(training_images, 
+					letter_keys, character_set=character_set, full_widths_only=True,
+					W=72, H=48)
+    print(char_list)
+    print(start_list)
+    print(stop_list)
+
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+for i in range(10):
+    img,char_list, = generate_test_example(training_images, 
+					letter_keys, character_set=character_set,
+					H=48, min_chars=2, max_chars=6)
+    print(char_list)
+
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
